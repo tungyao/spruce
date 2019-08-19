89545d676a09a6d9915179f6b1ac332dd8dc72a8
tungyao <1922527051@qq.com>
2019-08-19 23:13:04 +0800

    增加暴雪hash算法

--- core/sphash/shash.go
+++ core/sphash/shash.go
@@ -6,7 +6,7 @@ type Hash struct {
 	node  *Hash
 }
 
-var cryptTable [0x500]int
+var cryptTable [0x500]uint
 
 func NewHash() *Hash {
 	return new(Hash)
@@ -19,16 +19,16 @@ func (h *Hash) Get(key string) *Hash {
 }
 func PrepareCryptTable() {
 	var (
-		seed   int = 0x00100001
-		index1     = 0
-		index2     = 0
-		i          = 0
+		seed   uint = 0x00100001
+		index1      = 0
+		index2      = 0
+		i           = 0
 	)
-	for index1 = 1; index1 < 0x100; index1++ {
+	for index1 = 0; index1 < 0x100; index1++ {
 		for index2 = index1; i < 5; i++ {
 			var (
-				tp1 int
-				tp2 int
+				tp1 uint
+				tp2 uint
 			)
 			seed = (seed*125 + 3) % 0x2AAAAB
 			tp1 = (seed & 0xFFFF) << 0x10
@@ -37,23 +37,37 @@ func PrepareCryptTable() {
 			cryptTable[index2] = tp1 | tp2
 			index2 += 0x100
 		}
+		i = 0
+
 	}
 }
-func HashString(str []rune) int {
+func HashString(str []rune, hashtype uint) uint {
 	var (
-		key   = str
-		seed1 = 0x7FED7FED
-		seed2 = 0xEEEEEEEE
-		ch    int
+		key        = str
+		seed1 uint = 0x7FED7FED
+		seed2 uint = 0xEEEEEEEE
+		ch    uint
 	)
 	for _, v := range key {
 		va := v
+		ch = uint(va)
 		va++
-
-		ch = int(va)
-		seed1 = cryptTable[(1<<8)+ch] ^ (seed1 + seed2)
+		seed1 = cryptTable[(hashtype<<8)+ch] ^ (seed1 + seed2)
 		seed2 = ch + seed1 + seed2 + (seed2 << 5) + 3
 
 	}
 	return seed1
+}
+func GetHashPos(str []rune) {
+	var (
+		HASH_OFFSET uint = 0
+		HASH_A      uint = 1
+		HASH_B      uint = 2
+	)
+	var nHash int = int(HashString(str, HASH_OFFSET))
+	var nHashA int = int(HashString(str, HASH_A))
+	var nHashB int = int(HashString(str, HASH_B))
+	var nHashStart int = nHash % len(cryptTable)
+	var nHashPos = nHashStart
+
 }
--- test/hash_test.go
+++ test/hash_test.go
@@ -1,7 +1,7 @@
 package test
 
 import (
-	"fmt"
+	"../core/sphash"
 	"testing"
 )
 
@@ -11,16 +11,20 @@ type Data struct {
 }
 
 func TestHash(t *testing.T) {
-	//sphash.PrepareCryptTable()
-	//data:=sphash.HashString([]rune("username"))
-	//t.Log(data)
+	sphash.PrepareCryptTable()
+	data := sphash.HashString([]rune("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"), 1)
+	t.Log(data)
+	data2 := sphash.HashString([]rune("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"), 2)
+	t.Log(data2)
+	data3 := sphash.HashString([]rune("ccccccccccccccccccccccccccccccccccc"), 3)
+	t.Log(data3)
 	//runtime.GC()
-	var data [10000000]*Data
-	var ptr *Data
-	for i := 0; i < 10000000; i++ {
-		ptr = &Data{int32(i), int32(i)}
-
-		data[i] = ptr
-	}
-	_, _ = fmt.Scan()
+	//var data [10000000]*Data
+	//var ptr *Data
+	//for i := 0; i < 10000000; i++ {
+	//	ptr = &Data{int32(i), int32(i)}
+	//
+	//	data[i] = ptr
+	//}
+	//_, _ = fmt.Scan()
 }
