a63eed8abd551b752fb5fbee6d45e39edb77d23f
tungyao <1922527051@qq.com>
2019-08-20 19:06:19 +0800

    Merge branch 'developing_core' of https://github.com/tungyao/spruce into developing_core
    # Conflicts:
    #	.gitignore

--- core/sphash/shash.go
+++ core/sphash/shash.go
@@ -1,22 +1,50 @@
 package sphash
 
-type Hash struct {
+import (
+	"strings"
+)
+
+type Mapping struct {
+	hash  *Hash
+	deep  uint
 	key   string
 	value string
-	node  *Hash
+	node  map[string]*Mapping
+}
+type Hash struct {
+	isExists bool
+	hsa      uint
+	hsb      uint
+	node     map[string]string
 }
 
-var cryptTable [0x500]uint
+var cryptTable [0x500]uint   //TODO 用于算出hash值得数组
+var verticalTable [256]*Hash //TODO 用于存放hash 取余过后 得数值
+func NewMapping() *Mapping {
+	PrepareCryptTable()
+	return new(Mapping)
+}
+func NewNode(h *Hash, k string, v string, d uint) *Mapping {
+	return &Mapping{
+		hash:  h,
+		deep:  d,
+		key:   k,
+		value: v,
+		node:  make(map[string]*Mapping),
+	}
+}
+func (m *Mapping) Set(key string, value string) {
+	_, k := GetHashPos([]rune(key))
+	if verticalTable[k].node == nil {
+		verticalTable[k].node = make(map[string]string)
+	}
+	verticalTable[k].node[key] = value
+}
+func (m *Mapping) Get(key string) string {
+	_, k := GetHashPos([]rune(key))
+	return verticalTable[k].node[key]
+}
 
-func NewHash() *Hash {
-	return new(Hash)
-}
-func (h *Hash) Set(key string, value string) bool {
-	return false
-}
-func (h *Hash) Get(key string) *Hash {
-	return nil
-}
 func PrepareCryptTable() {
 	var (
 		seed   uint = 0x00100001
@@ -25,7 +53,7 @@ func PrepareCryptTable() {
 		i           = 0
 	)
 	for index1 = 0; index1 < 0x100; index1++ {
-		for index2 = index1; i < 5; i++ {
+		for index2, i = index1, 0; i < 5; index2 += 0x100 {
 			var (
 				tp1 uint
 				tp2 uint
@@ -35,10 +63,8 @@ func PrepareCryptTable() {
 			seed = (seed*125 + 3) % 0x2AAAAB
 			tp2 = seed & 0xFFFF
 			cryptTable[index2] = tp1 | tp2
-			index2 += 0x100
+			i += 1
 		}
-		i = 0
-
 	}
 }
 func HashString(str []rune, hashtype uint) uint {
@@ -46,28 +72,44 @@ func HashString(str []rune, hashtype uint) uint {
 		key        = str
 		seed1 uint = 0x7FED7FED
 		seed2 uint = 0xEEEEEEEE
-		ch    uint
+		ch    uint = 0
 	)
 	for _, v := range key {
-		va := v
-		ch = uint(va)
-		va++
+		ch = uint(strings.ToUpper(string(v))[0])
 		seed1 = cryptTable[(hashtype<<8)+ch] ^ (seed1 + seed2)
 		seed2 = ch + seed1 + seed2 + (seed2 << 5) + 3
-
 	}
 	return seed1
 }
-func GetHashPos(str []rune) {
+func GetHashPos(str []rune) (*Hash, uint) { //TODO 需要将计算过的hash值取余放入数组中
 	var (
-		HASH_OFFSET uint = 0
-		HASH_A      uint = 1
-		HASH_B      uint = 2
+		HashOffset uint = 0
+		HashA      uint = 1
+		HashB      uint = 2
+		nHash      uint = HashString(str, HashOffset)
+		nHashA     uint = HashString(str, HashA)
+		nHashB     uint = HashString(str, HashB)
+		nHashStart uint = nHash % uint(len(cryptTable))
+		nHashPos        = nHashStart % 256 //TODO 相当于经过了三次Hash,最终得出位置
 	)
-	var nHash int = int(HashString(str, HASH_OFFSET))
-	var nHashA int = int(HashString(str, HASH_A))
-	var nHashB int = int(HashString(str, HASH_B))
-	var nHashStart int = nHash % len(cryptTable)
-	var nHashPos = nHashStart
+	for i := 0; i < len(verticalTable); i++ {
+		if verticalTable[nHashPos] != nil {
+			if verticalTable[nHashPos].hsa == nHashA && verticalTable[nHashPos].hsb == nHashB {
+				return verticalTable[nHashPos], nHashPos
+			} else {
+				nHashPos = (nHashPos + 1) % 256
+
+			}
+			if nHashPos == nHashStart {
+				return nil, nHashPos
+			}
+		}
+	}
+	verticalTable[nHashPos] = &Hash{
+		isExists: true,
+		hsa:      nHashA,
+		hsb:      nHashB,
+	}
+	return verticalTable[nHashPos], nHashPos
 
 }
--- test/hash_test.go
+++ test/hash_test.go
@@ -5,26 +5,19 @@ import (
 	"testing"
 )
 
-type Data struct {
-	key   rune
-	value rune
-}
-
 func TestHash(t *testing.T) {
-	sphash.PrepareCryptTable()
-	data := sphash.HashString([]rune("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"), 1)
-	t.Log(data)
-	data2 := sphash.HashString([]rune("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"), 2)
-	t.Log(data2)
-	data3 := sphash.HashString([]rune("ccccccccccccccccccccccccccccccccccc"), 3)
-	t.Log(data3)
-	//runtime.GC()
+	sp := sphash.NewMapping()
+	//sp.Set("USERA", "PASSWORD")
+	//sp.Set("USERB", "PASSWORD")
+	//sp.Set("USERC", "PASSWORD")
+	//t.Log(sp.Get("USERC"))
+
 	//var data [10000000]*Data
 	//var ptr *Data
-	//for i := 0; i < 10000000; i++ {
-	//	ptr = &Data{int32(i), int32(i)}
-	//
-	//	data[i] = ptr
-	//}
+	d := "abcdefghij"
+	for i := 0; i < len(d)*1000000; i++ {
+		sp.Set(string(d[len(d)*1000000%10]), "PASSWORD")
+	}
+	//time.Sleep(time.Second*1)
 	//_, _ = fmt.Scan()
 }
